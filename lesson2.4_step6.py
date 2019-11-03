d2c6093c25a5926a94eeaca7a6e31c7daa429076
Merhallon <marina13.29@mail.ru>
2019-11-03 17:30:56 +0300

    Lesson 2.4 step 6 exeptions

---
+++ lesson2.4_step6.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import time
+import math
+from selenium.webdriver.support.ui import Select
+import os
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    time.sleep(1)
+
+    button = browser.find_element_by_class_name("btn")
+    button.click()
+
+    new_window = browser.window_handles[1]
+
+    browser.switch_to.window(new_window)
+
+    x2_element = browser.find_element_by_id("input_value")
+    x2 = x2_element.text
+    y = calc(x2)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    button = browser.find_element_by_tag_name("button")
+    button.click()
+    # Отправляем заполненную форму
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
